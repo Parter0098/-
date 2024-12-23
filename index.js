c699abc8603846630339f98f035ae62995e24af0
Saman <94233095+Parter0098@users.noreply.github.com>
2024-12-23 14:26:47 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let openModal = document.getElementById('openModal')
+let closeModal = document.getElementById('closeModal')
+let modal = document.getElementById('modal')
+
+openModal.addEventListener(
+  'click',
+  () => ((modal.style.display = 'flex'), (openModal.style.display = 'none'))
+)
+
+closeModal.addEventListener(
+  'click',
+  () => (
+    (modal.style.display = 'none'),
+    (openModal.style.display = 'block')
+  )
+)
+
+window.addEventListener('click', ({ target }) => {
+  if (target === modal) {
+    modal.style.display = 'none'
+  }
+})
